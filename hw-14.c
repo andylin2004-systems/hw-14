fc0117528ed079842e0af2e86a347e5c3c935d8a
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-11-09 12:17:42 -0500

    added sighandler

--- hw-14.c
+++ hw-14.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <signal.h>
+#include <fcntl.h>
+
+static void sighandler(int signo){
+    if (signo == SIGINT){
+        FILE *file = open("error.log", O_WRONLY | O_TRUNC);
+        write(file, "Operation terminated because of a SIGINT", sizeof("Operation terminated because of a SIGINT"));
+        exit(0);
+    }
+    else if (signo == SIGUSR1){
+        printf("Parent PID: %d\n", getppid());
+    }
+}
 
 int main(){
     while (1){
