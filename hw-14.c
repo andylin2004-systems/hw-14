341ed8a0e39189762cb30394c17b331b44ff515a
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-11-09 12:29:56 -0500

    fix issue where file failed to create and open and fix issue where signal calls were undetected

--- hw-14.c
+++ hw-14.c
@@ -2,10 +2,11 @@
 #include <unistd.h>
 #include <signal.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
 static void sighandler(int signo){
     if (signo == SIGINT){
-        FILE *file = open("error.log", O_WRONLY | O_TRUNC);
+        FILE *file = open("error.log", O_CREAT | O_WRONLY | O_TRUNC, 0644);
         write(file, "Operation terminated because of a SIGINT\n", sizeof("Operation terminated because of a SIGINT\n"));
         close(file);
         exit(0);
@@ -15,7 +16,10 @@ static void sighandler(int signo){
     }
 }
 
-int main(){
+int main()
+{
+    signal(SIGINT, sighandler);
+    signal(SIGUSR1, sighandler);
     while (1){
         printf("PID %d\n", getpid());
         sleep(1);
